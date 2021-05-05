9da255ccd04df7f8a371109b2532cb1aac1da221
Calvyn <calvyn360@gmail.com>
2021-05-04 21:40:05 -0400

    Implemented disappear on scroll with typescript

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 "use strict";
-let links = document.querySelectorAll(`.navlinks`);
-console.log(links);
+const links = document.querySelectorAll(`.navlinks`);
+const header = document.querySelector(`header`);
 links.forEach(function (item) {
     item.addEventListener(`click`, event => {
-        document.getElementById("nav-toggle").checked = false;
+        const input = document.getElementById("nav-toggle");
+        input.checked = false;
     });
+});
+// let header=
+let scrollPos = 0;
+window.addEventListener('scroll', function () {
+    // detects if it is scrolling up and makes it visible
+    if ((document.body.getBoundingClientRect()).top > scrollPos) {
+        header.style.opacity = "1";
+    }
+    else {
+        header.style.opacity = "0";
+    }
+    // detects if its scrolling down then makes it invisible
+    scrollPos = (document.body.getBoundingClientRect()).top;
 });
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
-let links = document.querySelectorAll(`.navlinks`) as NodeListOf<HTMLElement>
+const links = document.querySelectorAll(`.navlinks`) as NodeListOf<HTMLInputElement>
+const header = document.querySelector(`header`) as HTMLElement
 
-console.log(links);
 
 
-links.forEach(function (item) {
+links.forEach(function (item:HTMLInputElement) {
   item.addEventListener(`click`, event => {
-    document.getElementById("nav-toggle").checked = false;
+    const input: HTMLInputElement = document.getElementById("nav-toggle")
+    input.checked = false;
   })
   
 })
+
+// let header=
+
+let scrollPos = 0;
+
+window.addEventListener('scroll', function(){
+  // detects if it is scrolling up and makes it visible
+  if ((document.body.getBoundingClientRect()).top > scrollPos){
+    header.style.opacity = "1"
+
+  }
+	else{
+    header.style.opacity = "0"
+
+  }
+	// detects if its scrolling down then makes it invisible
+	scrollPos = (document.body.getBoundingClientRect()).top;
+});
 
